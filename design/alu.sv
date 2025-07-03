8b3dd51ca545a1185822fffaded0b05e366b628e
Jotaveesse <jvs2@cin.ufpe.br>
2025-07-03 16:45:59 -0300

    organização das operações da ALU

--- design/alu.sv
+++ design/alu.sv
@@ -17,9 +17,13 @@ module alu#(
             case(Operation)
                 4'b0000:        // AND
                         ALUResult = SrcA & SrcB;
-                4'b0010:        // ADD
+                4'b0001:        // OR
                         ALUResult = SrcA + SrcB;
-                4'b0100:        // SUB
+                4'b0010:        // XOR
+                        ALUResult = SrcA + SrcB;
+                4'b0100:        // ADD
+                        ALUResult = SrcA + SrcB;
+                4'b0101:        // SUB
                         ALUResult = SrcA - SrcB;
                 4'b1000:        // Equal
                         ALUResult = (SrcA == SrcB) ? 1 : 0;
