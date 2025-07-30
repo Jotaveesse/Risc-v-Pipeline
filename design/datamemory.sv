31bd165a6ee1b7d82f3dd746ff43d5ae87e9574d
Jotaveesse <jvs2@cin.ufpe.br>
2025-07-30 16:25:43 -0300

    correção dos interleaved banks

--- design/datamemory.sv
+++ design/datamemory.sv
@@ -17,6 +17,7 @@ module datamemory #(
   logic [31:0] waddress;
   logic [31:0] Datain;
   logic [31:0] Dataout;
+  logic [31:0] Datamiddle;
   logic [ 3:0] Wr;
 
   Memoria32Data mem32 (
@@ -29,48 +30,51 @@ module datamemory #(
   );
 
   always_ff @(*) begin
-    raddress = {{22{1'b0}}, a};
+    raddress = {{22{1'b0}}, {a[8:2], {2{1'b0}}}};
     waddress = {{22{1'b0}}, {a[8:2], {2{1'b0}}}};
     Datain = wd;
     Wr = 4'b0000;
 
     if (MemRead) begin
-      case (Funct3)
-        3'b000:  //LB
-        rd = {{24{Dataout[7]}}, Dataout[7:0]};
-        3'b001:  //LH
-        rd <= {{16{Dataout[15]}}, Dataout[15:0]};
-        3'b010:  //LW
-        rd <= Dataout;
-        3'b100:  //LBU
-        rd = {{24{1'b0}}, Dataout[7:0]};
+      case (a[1:0])
+        2'b00:  //LB
+        Datamiddle <= Dataout;
+        2'b01:  //LH
+        Datamiddle <= Dataout >> 8;
+        2'b10:  //LW
+        Datamiddle <= Dataout >> 16;
+        2'b11:  //LBU
+        Datamiddle <= Dataout >> 24;
         default: rd <= Dataout;
       endcase
-    end else if (MemWrite) begin
-      case (a[1:0])
-        2'b00:
-          Wr = 4'b0001;
-        2'b01:
-          Wr = 4'b0010;
-        2'b10:
-          Wr = 4'b0100;
-        2'b11:
-          Wr = 4'b1000;
-        default: Wr = 4'b0000;
+      case (Funct3)
+        3'b000:  //LB
+        rd = {{24{Datamiddle[7]}}, Datamiddle[7:0]};
+        3'b001:  //LH
+        rd <= {{16{Datamiddle[15]}}, Datamiddle[15:0]};
+        3'b010:  //LW
+        rd <= Datamiddle;
+        3'b100:  //LBU
+        rd = {{24{1'b0}}, Datamiddle[7:0]};
+        default: rd <= Datamiddle;
       endcase
-
+    end else if (MemWrite) begin
       case (Funct3)
         3'b000: begin  //SB
           Datain <= {{24{wd[31]}}, wd[7:0]};
+          Wr = 4'b0001;
         end
         3'b001: begin  //SH
           Datain <= {{16{wd[31]}}, wd[15:0]};
+          Wr = 4'b0011;
         end
         3'b010: begin  //SW
           Datain <= wd;
+          Wr = 4'b1111;
         end
         default: begin
           Datain <= wd;
+          Wr = 4'b1111;
         end
       endcase
     end
